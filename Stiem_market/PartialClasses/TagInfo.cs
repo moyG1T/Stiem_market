802b842398df1b9e277968134fd6f0cb0411ae71
Cool <146436146+moyG1T@users.noreply.github.com>
2024-04-08 22:14:10 +0300

    Тег теперь хранит надлежащую коллекцию игр

--- Stiem_market/PartialClasses/TagInfo.cs
+++ Stiem_market/PartialClasses/TagInfo.cs
@@ -1,9 +1,17 @@
-﻿namespace Stiem_market.PartialClasses
+﻿using Stiem_market.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stiem_market.PartialClasses
 {
     public class TagInfo
     {
         public int ID { get; set; }
         public string Title { get; set; }
         public int Count { get; set; }
+        public IEnumerable<Games> ContainingGames
+        {
+            get => App.db.Games.Where(g => g.GameTags.Any(gt => gt.Game_id == g.ID && gt.Tags.Title == Title)).Take(16).ToList();
+        }
     }
 }
